961f4320982f99c3e758bf356a638441734dc2cb
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-01-23 23:33:30 +0500

    index html

---
+++ js/script.js
@@ -0,0 +1,109 @@
+// 1-masal
+
+
+// let n = +prompt("soni kiriting");
+
+// let result = '';
+
+// switch (n) {
+//     case 1:
+//         result = "yanvar"
+//         break;
+//     case 2:
+//         result = "fevral"
+//         break;
+
+//     case 3:
+//         result = "mart"
+//         break;
+
+//     case 4:
+//         result = "aprel"
+//         break;
+
+//     case 5:
+//         result = "may"
+//         break;
+
+//     case 6:
+//         result = "iyun"
+//         break;
+//     case 7:
+//         result = "iyul"
+//         break;
+//     case 8:
+//         result = "avgust"
+//         break;
+//     case 9:
+//         result = "sentyabr"
+//         break;
+//     case 10:
+//         result = "oktyabr"
+//         break;
+//     case 11:
+//         result = "noyabr"
+//         break;
+//     case 12:
+//         result = "dekabr"
+//         break;
+
+//     default:
+//         result = "bunday oy mavjud emas"
+//         break;
+// }
+// console.log(result);
+
+
+
+// 2-masala
+
+
+// let n = +prompt("soni kiriting")
+
+// switch (n) {
+//     case 1:
+//         result = 'qish'
+//         break;
+//     case 2:
+//         result = 'qish'
+//         break;
+//     case 3:
+//         result = 'bahor'
+//         break;
+//     case 4:
+//         result = 'bahor'
+//         break;
+//     case 5:
+//         result = 'bahor'
+//         break;
+//     case 6:
+//         result = 'yoz'
+//         break;
+//     case 7:
+//         result = 'yoz'
+//         break;
+//     case 8:
+//         result = 'yoz'
+//         break;
+//     case 9:
+//         result = 'kuz'
+//         break;
+//     case 10:
+//         result = 'kuz'
+//         break;
+//     case 11:
+//         result = 'kuz'
+//         break;
+//     case 12:
+//         result = 'qish'
+//         break;
+
+//     default:
+//         result='bunday fasil yoq'
+//         break;
+// }
+
+// console.log(result);
+
+// 3-masala
+
